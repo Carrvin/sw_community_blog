2e021630be5e6fc923e06e9d6a8077eeeaa2bf56
pascal <pait@posteo.de>
2021-02-27 14:36:27 +0100

    added i18n

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+    <p>{{ $t('message', { msg: 'i18n world' }) }}</p>
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the

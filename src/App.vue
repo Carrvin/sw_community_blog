7eb5f04bed4e7d955243e8f8393caa5b19bfd1fd
pascal <pait@posteo.de>
2021-02-27 15:54:40 +0100

    added first draft of the view for new blog-entries

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/new-blog-entry">New Blog Entry</router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/NewBlogEntry.vue
@@ -0,0 +1,70 @@
+<template>
+
+  <div>
+    <b-container>
+      <b-card>
+        <template #header>
+          <h6 class="mb-0">{{ $t('newBlockEntry.overallTitleLabel') }}</h6>
+        </template>
+
+        <b-container>
+
+        <b-form-row>
+          <b-col sm="2">
+            <label>{{ $t('newBlockEntry.titleLabel') }}</label>
+          </b-col>
+          <b-col sm="10">
+            <b-form-input
+              id="form-input-title"
+              type="text"
+              v-model="title"
+              :placeholder="$t('newBlockEntry.titlePlaceholder')">
+            </b-form-input>
+          </b-col>
+        </b-form-row>
+
+        <b-form-row class="mt-2">
+          <b-col sm="2">
+            <label>{{ $t('newBlockEntry.contentLabel') }}</label>
+          </b-col>
+          <b-col sm="10">
+            <b-form-textarea
+              id="textarea-default"
+              v-model="content"
+              :placeholder="$t('newBlockEntry.contentPlaceholder')"
+              rows="10">
+            </b-form-textarea>
+          </b-col>
+        </b-form-row>
+
+        </b-container>
+
+        <template #footer>
+          <b-button
+            @click="submit"
+            variant="primary">
+            {{ $t('newBlockEntry.submitBtnLabel') }}
+          </b-button>
+        </template>
+      </b-card>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: '',
+      content: '',
+    };
+  },
+
+  methods: {
+    submit() {
+      console.log(this.title);
+      console.log(this.content);
+    },
+  },
+};
+</script>
